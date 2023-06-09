5a2f2f52ff6be671a748a1cc014a18120220d5cf
DiogoF22104684 <a22104684@alunos.ulht.pt>
2023-06-09 12:18:45 +0100

    Quick changes

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -7,23 +7,17 @@ public class GameManagerScript : MonoBehaviour
 {
     [SerializeField] private GameObject PlayerPrefab;
     [SerializeField] private CinemachineVirtualCameraBase camera;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.localPosition = new Vector3(-8, -3.5f, 0f);
         PlayerCheck();
     }
 
     private void PlayerCheck()
     {
-        bool playerDead = GameObject.FindGameObjectsWithTag("Player").Length == 0;
-        if(playerDead)
+        bool playerDead = GameObject.FindObjectOfType<Player_Movement>() == null;
+        if(playerDead) 
         {
             GameObject player = Instantiate(PlayerPrefab, gameObject.transform.localPosition, Quaternion.identity);
             camera.Follow = player.transform;
